ee3d27c941a73e53f9d0e5f594b1af6757747c50
Himanshu <himanshuchaudhery962@gmail.com>
2025-09-07 22:16:08 +0530

    Quater Work Left

--- C++/MSP.cpp
+++ C++/MSP.cpp
@@ -95,6 +95,69 @@ addItem(Bill b){
     }
 }
 
+// PRINT BILL
+printBill(Bill b){
+    system("cls");
+    int count = 0;
+    bool close = false;
+    while(!close){
+        system("cls");
+        int choice;
+        cout<<"\tAdd Bill."<<endl;
+        cout<<"\tClose Session"<<endl;
+        cout<<"\tEnter Choice: ";
+        cin>>choice;
+
+        if(choice==1){
+            string item;
+            int quant;
+            cout<<"\tEnter Item: ";
+            cin>>item;
+            cout<<"\tEnter Quantity";
+            cin>>quant;
+
+            ifstream in("D:/bill.txt"); // ifstream is used to read that data from file.
+            ofstream out("D:/bill temp.txt"); // ofstream is used to write a data in the file.
+
+            string line;
+            bool found = false;
+
+            while(getline(in, line)) // getline is function that read a line from txt file 
+            {
+                stringstream ss;
+                ss<<line;
+                string itemName;
+                int itemRate, itemQuant;
+                char delimiter;
+                ss>>itemName>>delimiter>>itemrate>>delimiter>>itemQuant;
+                
+                if(item==itemName){
+                    found = true;
+                    if(quant==itemQuant){
+                        int amount = itemRate * quant;
+                        cout<<"\t Item | Rate | Quantity | Amount"<<endl;
+                        cout<<"\t"<<itemName<<"\t "<<ItemRate<<"\t "<<quant<<"\t "<<amount<<endl;
+                        int newQuant = itemQuant - quant;
+                        itemQuant = newQuant;
+                        count +=amount;
+
+                        out<<"\t"<<itemName<<" : "<<itemRate<<" : "<<itemQuant<<endl;
+
+                    }
+                }
+                else{
+                    
+
+                }
+                else{
+                    // not item found
+
+                }
+            }
+        }
+    }
+}
+
 
 int main(){
     Bill b; // Object creation
@@ -115,6 +178,10 @@ int main(){
             system("cls");
             addItem(b);
             Sleep(3000);
+        }
+
+        else if(val==2){
+
         }
 
     }
